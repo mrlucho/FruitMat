c4e720ba412781e8c3b25357dbd39549956c8d7b
mrLucho <mluchowski@student.agh.edu.pl>
2022-08-10 20:45:01 +0200

    setup and instance

--- app/src/main/java/com/example/fruitmat/MainActivity.kt
+++ app/src/main/java/com/example/fruitmat/MainActivity.kt
@@ -6,17 +6,20 @@ import androidx.fragment.app.Fragment
 import com.example.fruitmat.FeatureCollectors.Domain.Manager.CollectorsManagerImpl
 import com.example.fruitmat.FeatureCollectors.presentation.Collectors
 import com.example.fruitmat.databinding.ActivityMainBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
     val manager = CollectorsManagerImpl(ArrayList())
     private lateinit var binding : ActivityMainBinding
+    private lateinit var mStorageRef: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
-
+        mStorageRef=  FirebaseStorage.getInstance().getReference();
 
         binding.bottomNavigationView.setOnItemSelectedListener {
 
